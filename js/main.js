b3827511af867c10d132ce4d0129a177324f0d0f
MelyIriarte <iriate.mela@gmail.com>
2022-03-21 22:23:26 -0300

    optimizador ternario aplicado

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     //VARIABLES
-    let baseDeDatos = [];
+
     let carrito = [];
     const divisa = "$";
-    const DOMgrid = document.getElementById("grid");
-    const DOMcarrito = document.querySelector("#carrito");
-    const DOMtotal = document.querySelector("#total");
-    const DOMbotonVaciar = document.querySelector("#boton-vaciar");
-    const DOMbotonComprar = document.querySelector("#boton-comprar");
     const miLocalStorage = window.localStorage;
 
     //PRODUCTOS
@@ -18,16 +13,14 @@ document.addEventListener('DOMContentLoaded', () => {
             descripcion: "Base de nuestro brownie de chocolate con una capa de dulce de leche y copos de merengue Italiano.",
             precio: 200,
             categoria: "tortas"
-        },
-        {
+        }, {
             id: 2,
             imagen: "img/chescake-min.png",
             nombre: "Cheescake",
             descripcion: " Base de galletitas trituradas con una crema de queso, terminada con una deliciosa jalea de frutos rojos.",
             precio: 3000,
             categoria: "tortas"
-        },
-        {
+        }, {
             id: 3,
             imagen: "img/rogel-min.png",
             nombre: "Rogel",
@@ -35,16 +28,14 @@ document.addEventListener('DOMContentLoaded', () => {
             precio: 2200,
             categoria: "tortas"
 
-        },
-        {
+        }, {
             id: 4,
             imagen: "img/lemonpie-min.png",
             nombre: "Lemonpie",
             descripcion: "Base de una masa fina  rellena de una crema de limon , cubierta con unos picos de merengue italiano",
             precio: 2500,
             categoria: "tortas"
-        },
-        {
+        }, {
             id: 5,
             imagen: "img/scon-min.png",
             nombre: "Scon",
@@ -131,8 +122,8 @@ document.addEventListener('DOMContentLoaded', () => {
             precio: 1200,
             categoria: "tortas"
         }
-
     ]
+
 
     //BUSCADOR
     const input1 = document.querySelector("#busqueda");
@@ -144,6 +135,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         contenedorBusqueda = document.querySelector("#grid");
         contenedorBusqueda.innerHTML = '';
+
         for (let producto of listadoProductos) {
             let nombre = producto.nombre.toLowerCase();
             if (nombre.indexOf(texto) != -1) {
@@ -155,59 +147,62 @@ document.addEventListener('DOMContentLoaded', () => {
                                                     <p> ${producto.descripcion}</p>
                                                     <p> $ ${producto.precio}</p>
                                                    `
-
                 const cardBoton = document.createElement("button");
                 cardBoton.classList.add("boton");
                 cardBoton.textContent = "Agregar";
                 cardBoton.setAttribute("marcador", producto.id);
-                cardBoton.addEventListener("click", añadirProductoAlCarrito);
+                cardBoton.addEventListener("click", ayadirProductoAlCarrito);
                 contenedorProducto.appendChild(cardBoton);
                 contenedorBusqueda.appendChild(contenedorProducto);
-
             }
         }
     }
 
     //FUNCION QUE CREA LAS CARDS
+    const domGrid = document.getElementById("grid");
+
     function renderizarProductos() {
+
         listadoProductos.forEach((info) => {
-            //Estructura 
+
             const card = document.createElement("article");
             card.classList.add("articleProductos");
-            //img
+
             const cardImg = document.createElement("img");
             cardImg.setAttribute("src", `${info.imagen}`);
-            //nombre
+
             const cardNombre = document.createElement("h5");
             cardNombre.textContent = `${info.nombre}`;
-            //descripcion
+
             const cardDescripcion = document.createElement("p");
             cardDescripcion.textContent = info.descripcion;
-            // precio
+
             const cardPrecio = document.createElement("p");
             cardPrecio.textContent = `$ ${info.precio}`;
-            //boton
+
             const cardBoton = document.createElement("button");
             cardBoton.classList.add("boton");
             cardBoton.textContent = "Agregar";
             cardBoton.setAttribute("marcador", info.id);
-            cardBoton.addEventListener("click", añadirProductoAlCarrito);
+            cardBoton.addEventListener("click", ayadirProductoAlCarrito);
             // armado de card
             card.appendChild(cardImg);
             card.appendChild(cardNombre);
             card.appendChild(cardDescripcion);
             card.appendChild(cardPrecio);
             card.appendChild(cardBoton);
-            DOMgrid.appendChild(card);
+            domGrid.appendChild(card);
 
         });
     }
 
     // añadir al carrito un producto, actualiza y guarda en el localstorage
-    function añadirProductoAlCarrito(e) {
+    function ayadirProductoAlCarrito(e) {
+
         carrito.push(e.target.getAttribute("marcador"));
         renderizarCarrito();
         guardarCarritoEnLocalStorage();
+
         const Toast = Swal.mixin({
             toast: true,
             position: "bottom-end",
@@ -219,29 +214,28 @@ document.addEventListener('DOMContentLoaded', () => {
                 toast.addEventListener('mouseleave', Swal.resumeTimer)
             }
         })
-
         Toast.fire({
             icon: 'success',
             title: 'Su producto se añadio al carrito'
         })
-
     }
+
     // muestra los productos guardados en el carrito
-    function renderizarCarrito() { //vacia todo el html
+    const domCarrito = document.querySelector("#carrito");
+    const domTotal = document.querySelector("#total");
 
-        DOMcarrito.textContent = "";
+    function renderizarCarrito() {
 
+        domCarrito.textContent = "";
         console.log(carrito);
-
         const carritoSinDuplicados = [...new Set(carrito)];
 
         carritoSinDuplicados.forEach((item) => {
             const miItem = listadoProductos.filter((itemBaseDatos) => {
                 return itemBaseDatos.id === parseInt(item);
             });
-
             const numeroDeVecesAcomprar = carrito.reduce((total, itemId) => {
-                return itemId === item ? total += 1 : total;
+                return itemId === item ? total += 1 : total; /* use operador ternario*/
             }, 0);
 
             const miNodo = document.createElement("li");
@@ -257,24 +251,22 @@ document.addEventListener('DOMContentLoaded', () => {
             miBoton.addEventListener('click', borrarItemCarrito);
 
             miNodo.appendChild(miBoton);
-            DOMcarrito.appendChild(miNodo);
+            domCarrito.appendChild(miNodo);
         })
-
-        DOMtotal.textContent = calcularTotal();
+        domTotal.textContent = calcularTotal();
     }
 
     //borra un elemento del carrito
     function borrarItemCarrito(e) {
-        //obtenemos el producto por su id
+
         const id = e.target.dataset.item;
-        //borra todos los productos
+
         carrito = carrito.filter((carritoId) => {
             return carritoId !== id;
         });
 
         renderizarCarrito();
         guardarCarritoEnLocalStorage();
-
     }
 
     //calcula el precio precio total
@@ -295,14 +287,12 @@ document.addEventListener('DOMContentLoaded', () => {
         renderizarCarrito();
         localStorage.clear();
 
-
         Swal.fire({
             title: '¿Esta seguro de querer vaciar este carrito?',
             showDenyButton: true,
             confirmButtonText: `Continuar comprando`,
             denyButtonText: 'Vaciar',
         }).then((result) => {
-            /* Read more about isConfirmed, isDenied below */
             if (result.isDenied) {
                 Swal.fire('Carrito vacio', '', 'success')
             }
@@ -328,21 +318,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function cargarCarritoDeLocalStorage() {
-
         if (miLocalStorage.getItem('carrito') !== null) {
-            // Carga la información al localstorage
             carrito = JSON.parse(miLocalStorage.getItem('carrito'));
         }
     }
 
-    // EVENTOS
-    DOMbotonVaciar.addEventListener('click', vaciarCarrito);
-    DOMbotonComprar.addEventListener("click", comprarCarrito);
+    const domBotonVaciar = document.querySelector("#boton-vaciar");
+    const domBotonComprar = document.querySelector("#boton-comprar");
 
+    domBotonVaciar.addEventListener('click', vaciarCarrito);
+    domBotonComprar.addEventListener("click", comprarCarrito);
 
-    // INICIALIZACION
     cargarCarritoDeLocalStorage();
     renderizarProductos();
     renderizarCarrito();
-
 });
